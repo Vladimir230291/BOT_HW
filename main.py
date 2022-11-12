3e83864a399f27a0207cedb056ac7de46d404ab0
Vladimir <lumen_08@mail.ru>
2022-11-12 11:45:53 +0300

    inform_sistem шinput-out

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import telebot
 from calculator import reformatNegativeValues, calculate, open_brackets
 from base import writeLog, readLog, clearLog
 from tic_tac_toe import *
+from InformSistem.base import *
 
-global wait_input
+global wait_input, i_s
 wait_input = False
+i_s = False
 
 token = 'token'
 bot = telebot.TeleBot(token)
@@ -22,12 +24,14 @@ def start_message(message):
                                       "/calc - запускает калькулятор\n"
                                       "/log - показать список операции\n"
                                       "/clear - очистить список операции\n"
-                                      "/game - запустить крестики-нолики\n")
+                                      "/game - запустить крестики-нолики\n"
+                                      "/inform_sistem - информационная система")
 
 
 @bot.message_handler(content_types='text')
 def all_message(message):
     global wait_input
+    global i_s
 
     if wait_input:
         if message.text != "/exit":
@@ -53,12 +57,16 @@ def all_message(message):
         bot.register_next_step_handler(msg, calc_result)
 
     if message.text == "/log":
-        lst = readLog()
-        bot.send_message(message.chat.id, lst)
+        try:
+            lst = readLog()
+            print(lst)
+            bot.send_message(message.chat.id, lst)
+        except:
+            bot.send_message(message.chat.id, "Список пуст")
 
     if message.text == "/clear":
-        lst = clearLog()
-        bot.send_message(message.chat.id, lst)
+        clearLog()
+        bot.send_message(message.chat.id, "список очищен!")
 
     if message.text == "/game":
         bot.send_message(message.chat.id, "игра запущена, введите число ячейки\n "
@@ -69,6 +77,20 @@ def all_message(message):
         bot.send_message(message.chat.id, x)
         bot.send_message(message.chat.id, next_stape())
         wait_input = True
+
+    if message.text == "/inform_sistem":
+        i_s = True
+        if i_s:
+            bot.send_message(message.chat.id, start_bd())
+            msg = bot.send_message(message.chat.id, "Введите нужную операцию:\n"
+                                                    "/add - добавить нового сотрудника\n"
+                                                    "/del - удалить сотрудника\n"
+                                                    "/all_db - показать полный список сотрудников\n"
+                                                    "/search - показать информацию по фамилии\n"
+                                                    "/out - выйти из системы")
+            bot.register_next_step_handler(msg, inform_sistem)
+
+
 
 
 def calc_result(mesg):
@@ -81,6 +103,21 @@ def calc_result(mesg):
     bot.send_message(mesg.chat.id, f"Ваш ответ: {result}")
 
 
+
+def inform_sistem(message):
+    global i_s
+    if message.text == "/add":
+        pass # добавление сотрудника
+    if message.text == "/del":
+        pass # удаление сотрудника
+    if message.text == "/all_db":
+        pass # viev all worker
+    if message.text == "/search":
+        pass # search worker in data bese last_name
+    if message.text == "/out":
+        bot.send_message(message.chat.id, "Закрыто, введите команду  /start")
+        i_s = False
+        return i_s
 try:
     print("Бот запущен")
     bot.polling()
